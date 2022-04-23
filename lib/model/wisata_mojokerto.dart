8ad602746f92d2b1ba990abb11f1de2ee32c3e25
Dimas Arif Setyawan <dimasarifsetyawan@gmail.com>
2022-04-23 21:53:37 +0700

    up to github

---
+++ lib/model/wisata_mojokerto.dart
@@ -0,0 +1,14 @@
+class WisataMojokerto{
+  String name;
+  String description;
+  String imageAsset;
+  String location;
+  List<String> imageUrls;
+
+  WisataMojokerto(
+      {required this.name,
+        required this.description,
+        required this.location,
+        required this.imageAsset,
+        required this.imageUrls});
+}
---
+++ lib/wisataMojokertoList.dart
@@ -0,0 +1,80 @@
+import 'model/wisata_mojokerto.dart';
+
+var wisataMojokerto = [
+  //1
+  WisataMojokerto(
+    name: 'Candi Bajang Ratu',
+    description: 'Ingin berpiknik bersama keluarga sekaligus mengingat sejarah jaman dahulu, tenang saja semuanya dapat anda lakukan di Candi Bajang Ratu. Lokasi wisata satu ini juga tak jauh dari wisata museum Balai Pelestarian Purbakala sehingga anda dapat mengunjungi dua tempat wisata sekaligus. Candi ini sendiri di kelilingi oleh taman yang indah, hijau dan menyegarkan sehingga sangat nyaman untuk dikunjungi. Menariknya lagi, berbagai sudut dari lokasi wisata ini sangat instagramable, jadi anda nantinya tak akan bosan untuk menelusuri sudut demi sudut tempat wisata ini.',
+    location: 'Jalan Candi Tikus No.9, Trowulan, Temon, Mojokerto, Jawa Timur',
+    imageAsset: 'images/bajangratu/bajang_ratu.jpg',
+    imageUrls: [
+      'images/bajangratu/bajang_ratu.jpg',
+      'images/bajangratu/bajang_ratu2.jpg',
+      'images/bajangratu/bajang_ratu3.jpg',
+      'images/bajangratu/bajang_ratu4.jpg',
+    ],
+ ),
+  //2
+  WisataMojokerto(
+    name: 'Candi Brahu',
+    description: 'Candi ini merupakan candi peninggalan Budha yang konon katanya mempunyai usia yang lebih tua dari kerajaan Majapahit sendiri. Candi Brahu sudah ada sejak saat Majapahit dipimpin oleh Hayam Wuruk, dan bahkan candi ini sudah ada ketika masa pemerintahan Raja Brawijaya I. Oleh karena itu, Sejarah Candi Brahu di perkirakan merupakan candi yang pertama kali dibangun di atas situs sejarah di trowulan. Dari segi bentuknya sendiri, candi Brahu dapat dikatakan berbeda dengan candi-candi yang terletak di Trowulan. Dimana hampir semua candi di trowulan di bangun ketika masa pemerintahan kerajaan majapahit. Jadi cukup menarik untuk dikunjungi ketika anda berada di Trowulan, Mojokerto.',
+    location:'Sawah, Bejijong, Trowulan, Mojokerto, Jawa Timur' ,
+    imageAsset: 'images/brahu/brahu.jpg',
+    imageUrls: [
+      'images/brahu/brahu.jpg',
+      'images/brahu/brahu2.png',
+      'images/brahu/brahu3.png',
+      'images/brahu/brahu4.png',
+  ],
+  ),
+  // 3
+  WisataMojokerto(
+    name: 'Patung Budha Tidur',
+    description: '"Patung Buddha Tidur berukuran raksasa yang konon katanya merupakan patung Buddha terbesar ketiga di dunia setelah patung serupa yang ada di Thailand dan Nepal. Patung ini sendiri memiliki ukuran panjang yang mencapai 22 meter, dengan lebar 6 meter, serta tinggi yang tak kurang dari 4,5 meter. Bagi kalain para instagramer, pastinya tempat wisata ini akan memanjakan anda dengan berbagai spot foto instagenic yang berada di sana.',
+    location: 'Jl. Raya Trowulan, Siti Inggil, Bejijong, Trowulan, Mojokerto, Jawa Timur',
+    imageAsset: 'images/budhatidur/budha_tidur.jpg',
+    imageUrls: [
+      'images/budhatidur/budha_tidur.jpg',
+      'images/budhatidur/budha_tidur2.png',
+    ],
+  ),
+  // 4
+  WisataMojokerto(
+    name: 'Air Terjun Dlundung',
+    description: 'Dengan ketinggian 14 meter, air terjun ini akan menyambut anda dengan butiran-butiran air yang jatuh dari puncak tebing. Letaknya yang berada di daerah lereng Gunung Welirang juga membuat suasana di sekitar air terjun sangat segar dan menenangkan. Di sekitar areal air terjun juga sering digunakan sebagai Bumi Perkemahan dimana biasanya akan digunakan untuk berakhir pekan bersama keluarga tercinta',
+    location: 'Desa Kemloko, Kecamatan Trawas, Kabupaten Mojokerto, Jawa Timur',
+    imageAsset: 'images/dlundung/dlundung.jpg',
+    imageUrls: [
+      'images/dlundung/dlundung.jpg',
+      'images/dlundung/dlundung2.png',
+      'images/dlundung/dlundung3.png',
+      'images/dlundung/dlundung4.png',
+    ],
+  ),
+  // 5
+  WisataMojokerto(
+    name: 'Museum Trowulan',
+    description: 'Museum ini diyakini sudah berusiasekitar 200 abad yang sekarang digunakan sebagai tempat untuk menyimpan artefak dan reruntuhan candi yang ditemukan Sir Thomas Stamford Raffles pada abad ke 19. Museum ini di dominasi oleh benda atau artefak koleksi dari peninggalan kerajaan Majapahit. Selain dari kerajaan Majapahit, di museum ini juga terdapat penemuan-penemuan artefak dari kerajaan Kahuripan, Kediri, dan juga Singosari. Museum ini selain digunakan sebagai tempat penyimpanan benda bersejarah, namun museum ini juga berperan sebagai tempat studi sejarah. Sehingga para pengunjungnya dapat melihat hasil-hasil kebudayaan jaman kuno melalui koleksi-koleksi dalam museum ini.',
+    location: 'Trowulan, Jalan Pendopo Agung, Trowulan, Mojokerto, Jawa Timur',
+    imageAsset: 'images/museumtrowulan/museum_trowulan.jpg',
+    imageUrls: [
+      'images/museumtrowulan/museum_trowulan.jpg',
+      'images/museumtrowulan/museum_trowulan2.png',
+      'images/museumtrowulan/museum_trowulan3.png',
+      'images/museumtrowulan/museum_trowulan4.png',
+    ],
+  ),
+  // 6
+  WisataMojokerto(
+    name: 'Pemandian Air Panas Padusan Pacet',
+    description: 'Obyek wisata yang satu ini lain dari yang lain pasalnya pemandian ini bukan sedikit unik karena merukan pemandian air panas yang bertemakan air terjun sebagai daya tarik utamanya. Sumber mata air dari tempat ini berasal dari mata air alami dari Gunung Welirang. Nikmati sensasi berendam di tempat ini dapat anda gunakan sebagai relaksasi tubuh yang telah lelah melakukan rutinitas sehari-hari, dan konon katanya air yang berada disini juga mampu mengobati berbagai macam penyakit kulit dan sudah sangat terkenal se antero Jawa Timur.',
+    location: 'Jl. Pemandian Air Panas Blok NO. 02 Pandusan, Padusan, Pacet, Mojokerto, Jawa Timur',
+    imageAsset: 'images/padusan/padusan.jpg',
+    imageUrls: [
+      'images/padusan/padusan.jpg',
+      'images/padusan/padusan2.png',
+      'images/padusan/padusan3.png',
+      'images/padusan/padusan4.png',
+    ],
+  ),
+];
